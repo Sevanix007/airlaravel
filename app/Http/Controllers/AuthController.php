3b7b1a253514b60334fba84ffcc043f9b2c313bc
Femboyphoby <116908016+Sevanix007@users.noreply.github.com>
2025-10-20 22:18:53 +0300

    Create AuthController.php

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+class loginn extends Controller
+{
+    //
+
+    public function register(Request $request)
+    {
+        $credentials = $request->validate([
+            'name' => 'required|string|max:255',
+            'password' => 'required',
+            'email' => 'required',
+        ]);
+
+        // Create the user
+        $user = User::create([
+            'name' => $credentials['name'],
+            'email' => $credentials['email'],
+            'password'=>Hash::make($credentials['password']),
+        ]);
+
+        // Log the user in
+ 
+
+        // Redirect to a desired location, e.g., home page
+        return back()->with('success', 'Registration successful!'); 
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+        $username = $request->input('login_email');
+        $password = $request->input('login_password');
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/'); // Redirect to intended page or home
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+
+
+}
