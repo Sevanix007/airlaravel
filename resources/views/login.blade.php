d8472cc5e6b211e4ff4725925a5490cbceb219c6
Femboyphoby <116908016+Sevanix007@users.noreply.github.com>
2025-10-21 08:57:36 +0300

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,7 @@
 @extends ('layouts.app')
 
 @section('content')
-link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1
 .3/css/bootstrap.min.css"/>
 <link rel="stylesheet" href="{{ asset('public/login.css') }}">
