bcc50360e9f9b544593347821e3d6e6d414afc48
Femboyphoby <116908016+Sevanix007@users.noreply.github.com>
2025-10-21 11:48:14 +0300

    Update detailsLidojums.blade.php

--- resources/views/detailsLidojums.blade.php
+++ resources/views/detailsLidojums.blade.php
@@ -6,66 +6,27 @@
 
     <div class="alert alert-info">
         <h4>
-            ID : {{ $lidojums->LidojumaID }}
-            | Numurs : {{ $lidojums->LidojumaNumurs }}
+            ID: {{ $lidojums->LidojumaID }} |
+            Numurs: {{ $lidojums->LidojumaNumurs }}
         </h4>
 
-        <p>
-            Izlidošanas lidosta :
-            {{ $lidojums->izlidesLidosta->LidostasNosaukums ?? 'ID: '.$lidojums->IzlidesLidosta }}
-        </p>
+        <p>Izlides Lidosta ID: {{ $lidojums->IzlidesLidosta }}</p>
+        <p>Ielides Lidosta ID: {{ $lidojums->IelidesLidosta }}</p>
+        <p>Izlides Laiks: {{ $lidojums->IzlidesLaiks }}</p>
+        <p>Ielides Laiks: {{ $lidojums->IelidesLaiks }}</p>
+        <p>Lidmašīnas ID: {{ $lidojums->LidmasinasID }}</p>
+        <p>Pilots1 ID: {{ $lidojums->Pilots1 }}</p>
+        <p>Pilots2 ID: {{ $lidojums->Pilots2 }}</p>
+        <p>Darbinieks1 ID: {{ $lidojums->Darbinieks1 }}</p>
+        <p>Darbinieks2 ID: {{ $lidojums->Darbinieks2 }}</p>
 
-        <p>
-            Ielidošanas lidosta :
-            {{ $lidojums->ielidesLidosta->LidostasNosaukums ?? 'ID: '.$lidojums->IelidesLidosta }}
-        </p>
-
-        <p>
-            Izlides laiks :
-            {{ \Carbon\Carbon::parse($lidojums->IzlidesLaiks)->format('Y-m-d H:i') ?? $lidojums->IzlidesLaiks }}
-        </p>
-
-        <p>
-            Ielides laiks :
-            {{ \Carbon\Carbon::parse($lidojums->IelidesLaiks)->format('Y-m-d H:i') ?? $lidojums->IelidesLaiks }}
-        </p>
-
-        <p>
-            Lidmašīna :
-            {{ $lidojums->lidmasina->RegNumurs ?? 'ID: '.$lidojums->LidmasinasID }}
-            {{ $lidojums->lidmasina->LidmasinasModelis ? ' ('.$lidojums->lidmasina->LidmasinasModelis.')' : '' }}
-        </p>
-
-        <p>
-            Pilots 1 :
-            {{ $lidojums->pilots1->Vards .' '. $lidojums->pilots1->Uzvards ?? 'ID: '.$lidojums->Pilots1 }}
-        </p>
-
-        <p>
-            Pilots 2 :
-            {{ $lidojums->pilots2 ? ($lidojums->pilots2->Vards .' '. $lidojums->pilots2->Uzvards) : ($lidojums->Pilots2 ? 'ID: '.$lidojums->Pilots2 : 'Nav') }}
-        </p>
-
-        <p>
-            Darbinieks 1 :
-            {{ $lidojums->darbinieks1->Vards .' '. $lidojums->darbinieks1->Uzvards ?? 'ID: '.$lidojums->Darbinieks1 }}
-        </p>
-
-        <p>
-            Darbinieks 2 :
-            {{ $lidojums->darbinieks2 ? ($lidojums->darbinieks2->Vards .' '. $lidojums->darbinieks2->Uzvards) : ($lidojums->Darbinieks2 ? 'ID: '.$lidojums->Darbinieks2 : 'Nav') }}
-        </p>
-
-        <a class="btn btn-danger" href="/lidojums/{{ $lidojums->LidojumaID }}/delete"> Dzēst </a>
-        <a class="btn btn-warning" href="/lidojums/{{ $lidojums->LidojumaID }}/edit"> Rediģēt </a>
-
-        <br><br>
+        <a class="btn btn-danger" href="/lidojums/{{ $lidojums->LidojumaID }}/delete">Dzēst</a>
+        <a class="btn btn-warning" href="/lidojums/{{ $lidojums->LidojumaID }}/edit">Rediģēt</a>
     </div>
 
     @if(session('success1'))
-    <div class="alert alert-danger">
-        {{ session('success1') }}
-    </div>
+        <div class="alert alert-success">
+            {{ session('success1') }}
+        </div>
     @endif
-
     @endsection
